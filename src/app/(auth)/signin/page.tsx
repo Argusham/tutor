e54259dfdc66a90ca41c29028afbc0b58399ee11
Viggo2001 <viggoent@gmail.com>
2025-04-12 20:49:33 +0200

    signin and signup ui

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -12,13 +12,16 @@ const SignIn = () => {
     return (
         <div className="h-[100vh] content-center">
             <form className="grid grid-cols-1 shadow-lg p-15 rounded-md mx-auto gap-5 max-w-[30%]">
-                <div className="text-center grid grid-cols-2 mb-4">
-                    <h1 className={`text-2xl pointer ${form === formType.LOGIN ? 'text-red-500' : 'text-black'}`} onClick={() => setForm(formType.LOGIN)}>Log in</h1>
-                    <h1 className={`text-2xl ${form === formType.SIGNUP ? 'text-red-500' : 'text-black'}`} onClick={() => setForm(formType.SIGNUP)}>Sign up</h1>
+                <div className="text-center grid grid-cols-2 mb-4 gap-2 p-2 rounded-md bg-gray-100">
+                    <h1 className={`text-xl cursor-pointer py-5 rounded-md ${form === formType.LOGIN ? 'bg-red-200 text-white' : 'bg-white text-black'}`} onClick={() => setForm(formType.LOGIN)}>Log in</h1>
+                    <h1 className={`text-xl cursor-pointer py-5 rounded-md ${form === formType.SIGNUP ? 'bg-blue-200 text-white' : 'bg-white text-black'}`} onClick={() => setForm(formType.SIGNUP)}>Sign up</h1>
                 </div>
-                <input type="text" placeholder="Email" className="p-5 rounded-md border" />
-                <input type="password" placeholder="Password" className="p-5 rounded-md border" />
-                <button className="bg-blue-200 p-5 rounded-md" type="submit">Log In</button>
+                <input type="text" placeholder="Email" className="p-5 rounded-md border border-gray-400" />
+                <input type="password" placeholder="Password" className="p-5 rounded-md border border-gray-400" />
+                {form === formType.SIGNUP && (
+                     <input type="password" placeholder="Confirm Password" className="p-5 rounded-md border border-gray-400" />
+                )}
+                <button className="bg-blue-200 p-5 rounded-md text-white text-xl" type="submit">Log In</button>
                 <span className="text-center">Forgot Password?</span>
             </form>
         </div>
